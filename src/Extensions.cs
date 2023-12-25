1dcf78c41cd445893993e003f1cd26120ab36cdf
NoirCatto <thesoaringcatto@gmail.com>
2023-12-26 00:53:38 +0100

    Organise Extensions

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -1,15 +1,33 @@
 using System;
+using UnityEngine;
 
 namespace TheFriend;
 
 public static class Extensions
 {
+    #region RainWorld Specific
     public static bool IsSolaceName(this SlugcatStats.Name name)
     {
         return name.value == Plugin.FriendName.value || name.value == Plugin.NoirName.value || name.value == Plugin.DragonName.value || name.value == Plugin.DelugeName.value ||
                name.value == Plugin.BelieverName.value;
     }
 
+    public static bool IsSmallerThanMe(this Player self, Creature crit) => crit.Template.smallCreature || self.TotalMass > crit.TotalMass;
+    public static bool IsSmallerThanMe(this Creature self, Creature crit) => self.TotalMass > crit.TotalMass;
+    #endregion
+
+    #region Custom values extensions
+    public static float MaxValue(this Vector2 vector)
+    {
+        return Mathf.Max(vector.x, vector.y);
+    }
+    public static float MinValue(this Vector2 vector)
+    {
+        return Mathf.Min(vector.x, vector.y);
+    }
+    #endregion
+
+    #region Built-in values extensions
     public static float Map(this float x, float in_min, float in_max, float out_min, float out_max, bool clamp = false)
     {
         if (clamp) x = Math.Max(in_min, Math.Min(x, in_max));
@@ -20,9 +38,9 @@ public static class Extensions
     {
         if (counter > 0) counter--;
     }
+    #endregion
 
-    public static bool IsSmallerThanMe(this Player self, Creature crit) => crit.Template.smallCreature || self.TotalMass > crit.TotalMass;
-    public static bool IsSmallerThanMe(this Creature self, Creature crit) => self.TotalMass > crit.TotalMass;
+    #region Reflection //Heavy operations involving the use of System.Reflection
 
     /// <summary>
     /// Returns the name of the getter method for the property
@@ -50,4 +68,6 @@ public static class Extensions
             throw new ArgumentNullException($"Property {propertyName} of {type.Name} returned null!");
         return result;
     }
+
+    #endregion
 }
