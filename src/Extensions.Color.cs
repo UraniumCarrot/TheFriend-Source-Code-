035a81d08e29f0cca101ac155a17980f01ce9272
UraniumCarrot <aroadrunnercuckoo@gmail.com>
2024-01-09 14:04:28 -0800

    ChangeHue now accomodates negative numbers

--- src/Extensions.Color.cs
+++ src/Extensions.Color.cs
@@ -14,7 +14,7 @@ public partial class Extensions
     public static void ChangeHue(this ref Color color, float newHue)
     {
         var colorHsl = Custom.RGB2HSL(color);
-        colorHsl.x = newHue - Mathf.Floor(newHue);
+        colorHsl.x = Mathf.Abs(newHue - Mathf.Floor(newHue));
         color = colorHsl.HSL2RGB();
     }
 
