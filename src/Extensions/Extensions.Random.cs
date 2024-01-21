7c2f4f08cf2e2e333bc7fddb24aee04a0d1651f9
Aissurtievos <Traviz1232@gmail.com>
2024-01-21 13:16:03 -0500

    Change GenerateRandomList & WeightedItem to use doc comments, change WeightedItem to struct

--- src/Extensions/Extensions.Random.cs
+++ src/Extensions/Extensions.Random.cs
@@ -5,24 +5,40 @@ using UnityEngine;
 namespace TheFriend;
 public partial class Extensions
 {
-    // Data helper type for GenerateRandomList
-    // Item: The element this entry represents
-    // Weight: Roughly how much this item should be worth when calculating vs other elements
-    // Limit: The maxiumum amount of this element you want to see in the output list
-    // Weight is multiplied by Limit when used in the calculation, so be sure to adjust the weight accordingly or items with higher limits might appear more often than expected
-    public class WeightedItem<T>
+    /// <summary>
+    /// Data helper type for GenerateRandomList
+    /// </summary>
+    /// <remarks>
+    /// <para>Note: Weight is multiplied by Limit when used in the calculation, so be sure to adjust the weight accordingly or items with higher limits might appear more often than expected</para>
+    /// </remarks>
+    public struct WeightedItem<T>
     {
+        /// <summary>
+        /// The element this entry represents
+        /// </summary>
         public T Item;
+        /// <summary>
+        /// Roughly how much this item should be worth when calculating vs other elements
+        /// </summary>
         public float Weight;
+        /// <summary>
+        /// The maximum amount of this element you want to see in the output list
+        /// </summary>
         public int Limit;
     }
 
-    // Generates a list of random elements sourced from the input list parameter "entries"
-    // Percent chance for each item to be picked is ((Limit * Weight) / totalWeight) * 100 where totalWeight is all of the individual (Limit * Weight) in the list summed together
-    // When an item with more than 1 as its limit is selected, its Limit will be reduced by 1, and its chances will be reduced accordingly (with others being increased due to this)
-    // Items with 1 as the Limit will simply be taken out of the equation once selected, as 0 (Limit*Weight) will remove their chance of being selected.
-    // The length of the output list is set using the "count" parameter
-    // An option seed parameter can be used if reproducable results are desired.
+    /// <summary>
+    /// Generates a list of random elements sourced from the input list parameter "entries"
+    /// </summary>
+    /// <param name="entries">The list of weighted items</param>
+    /// <param name="count">The number of items to generate</param>
+    /// <param name="seed">An optional seed for reproducible results</param>
+    /// <returns>"count" randomly selected items from the "entries" input list</returns>
+    /// <remarks>
+    /// <para>Percent chance for each item to be picked is ((Limit * Weight) / totalWeight) * 100 where totalWeight is all of the individual (Limit * Weight) in the "entries" list summed together</para>
+    /// <para>When an item with more than 1 as its limit is selected, its Limit will be reduced by 1, and its chances will be reduced accordingly (with others being increased due to this)</para>
+    /// <para>Example: {Limit = 3, Weight = 10} will be worth 30 weight, after one has been chosen, it will now only be worth 20 weight.</para>
+    /// </remarks>
     public static IList<T> GenerateRandomList<T>(IList<WeightedItem<T>> entries, int count, int? seed = null)
     {
         // Check for empty list
@@ -56,7 +72,7 @@ public partial class Extensions
             .Select(entry => entry.Limit)
             .ToList();
 
-        List<T> outputList = new();
+        List<T> outputList = [];
 
         // select each item
         for (var i = 0; i < count; i++)
