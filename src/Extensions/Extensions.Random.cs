ec52787f748dba01efa1457f35a2968d4a70f95d
Aissurtievos <Traviz1232@gmail.com>
2024-01-24 21:04:05 -0500

    Rework GenerateRandomList() to have an optional "target" parameter that it will try to reach, but not exceed.

--- src/Extensions/Extensions.Random.cs
+++ src/Extensions/Extensions.Random.cs
@@ -11,97 +11,151 @@ public partial class Extensions
     /// <remarks>
     /// <para>Note: Weight is multiplied by Limit when used in the calculation, so be sure to adjust the weight accordingly or items with higher limits might appear more often than expected</para>
     /// </remarks>
-    public struct WeightedItem<T>
+    public class WeightedItem<T>
     {
         /// <summary>
         /// The element this entry represents
         /// </summary>
         public T Item;
         /// <summary>
-        /// Roughly how much this item should be worth when calculating vs other elements
+        /// Roughly how often this item should appear vs other elements
         /// </summary>
         public float Weight;
         /// <summary>
         /// The maximum amount of this element you want to see in the output list
         /// </summary>
         public int Limit;
+        /// <summary>
+        /// How much this item contributes to a running total
+        /// </summary>
+        public float? Contribution;
     }
 
     /// <summary>
     /// Generates a list of random elements sourced from the input list parameter "entries"
     /// </summary>
-    /// <param name="entries">The list of weighted items</param>
+    /// <param name="inputList">The list of weighted items to use</param>
     /// <param name="count">The number of items to generate</param>
     /// <param name="seed">An optional seed for reproducible results</param>
-    /// <returns>"count" randomly selected items from the "entries" input list</returns>
+    /// <param name="target">A maximum target "Contribution" value that the function will attempt to reach, but never exceed.</param>
+    /// <returns>A list of randomly selected items from the "entries" input list</returns>
     /// <remarks>
     /// <para>Percent chance for each item to be picked is ((Limit * Weight) / totalWeight) * 100 where totalWeight is all of the individual (Limit * Weight) in the "entries" list summed together</para>
     /// <para>When an item with more than 1 as its limit is selected, its Limit will be reduced by 1, and its chances will be reduced accordingly (with others being increased due to this)</para>
     /// <para>Example: {Limit = 3, Weight = 10} will be worth 30 weight, after one has been chosen, it will now only be worth 20 weight.</para>
+    /// <para>When "lowerTarget" and "upperTarget" is used, "count" is the max amount of creatures that will be used to reach the target value</para>
+    /// <para>Otherwise, the function just generates the requested count.</para>
     /// </remarks>
-    public static IList<T> GenerateRandomList<T>(IList<WeightedItem<T>> entries, int count, int? seed = null)
+    public static IList<T> GenerateRandomList<T>(IList<WeightedItem<T>> inputList, int count, int? seed = null, float? target = null)
     {
         // Check for empty list
-        if (count <= 0)
-        {
-            Plugin.LogSource.LogMessage($"GenerateRandomList() asked to generate empty list? count = 0");
-            return new List<T>();
-        }
+        if (count <= 0) return [];
 
-        // Check if we can reach the count using the items in the list, otherwise log error and clamp the generated count
-        var maxPossibleCount = entries.Sum(item => item.Limit);
+        // Check if we can reach the count using the items in the list, otherwise clamp the generated count
+        var maxPossibleCount = inputList.Sum(item => item.Limit);
         if (count > maxPossibleCount)
         {
             Plugin.LogSource.LogError($"GenerateRandomList(): given a count that is higher than is possible using the supplied list! Count: {count}, Max Possible: {maxPossibleCount}, Clamping output to {maxPossibleCount} nodes!");
             count = maxPossibleCount;
         }
 
+        // Make a deep copy of inputList (please tell me if an easier way exists to do this)
+        List<WeightedItem<T>> entries = inputList
+            .Select(x => new WeightedItem<T>
+            {
+                Item = x.Item,
+                Weight = x.Weight,
+                Limit = x.Limit,
+                Contribution = x.Contribution
+            })
+            .ToList();
+
         // create the initial total weight, Add together all weights, taking into consideration the limit of each item
         var totalWeight = entries.Sum(item => item.Weight * item.Limit);
+        var totalValue = 0f;
 
-        // Set the RNG seed if we want to make reproducable results
-        Random.State? state = null;
-        if (seed != null)
-        {
-            state = Random.state;
-            Random.InitState(seed.Value);
-        }
-
-        // make a copy of the limits so we do not modify the original
-        var limits = entries
-            .Select(entry => entry.Limit)
-            .ToList();
+        // Set the RNG seed if we want to make reproducible results
+        var state = SetRngSeed(seed);
 
         List<T> outputList = [];
 
         // select each item
+
         for (var i = 0; i < count; i++)
         {
-            var threshold = Random.Range(0, totalWeight);
-            var acc = 0f; // accumulator
+            // Exit if we empty the entries list early
+            if (!entries.Any())
+                break;
 
-            // Find the first item that is above the threshold, and add it to the output list
-            // added items are removed from future selection by decrementing their limit & removing their weight from the total
-            for (var j = 0; j < entries.Count; j++)
+            var item = GetRandomEntry(entries, totalWeight);
+
+            if (target != null)
             {
-                var entry = entries[j];
-                if (acc + entry.Weight * limits[j] > threshold)
+                // Check for null contributions, and remove them if so
+                if (item.Contribution == null)
                 {
-                    limits[j] -= 1;
-                    totalWeight -= entry.Weight;
-                    outputList.Add(entry.Item);
-                    break;
+                    Plugin.LogSource.LogError(
+                        $"GenerateRandomList(): An item with a null \"Contribution\" value was found while using targets, removing item from future selection in this function call...");
+                    totalWeight -= item.Weight * item.Limit;
+                    entries.Remove(item);
+                    i--;
+                    continue;
                 }
-                acc += entry.Weight * limits[j];
+
+                // If this item would put us over the target, skip it and remove it from further selection, so that they dont get in the way of items that wouldn't
+                if (totalValue + item.Contribution > target)
+                {
+                    totalWeight -= item.Weight * item.Limit;
+                    entries.Remove(item);
+                    i--;
+                    continue;
+                }
+
+                totalValue += item.Contribution.Value;
             }
+
+            item.Limit -= 1;
+            totalWeight -= item.Weight;
+
+            outputList.Add(item.Item);
+
+            if (item.Limit == 0)
+                entries.Remove(item);
         }
 
         // If we modified the RNG using a seed, restore it back to what it was beforehand
         if (state != null)
-        {
             Random.state = state.Value;
-        }
 
         return outputList;
+    }
+
+    /// <summary>
+    /// Gets a random entry from the supplied list of entries
+    /// </summary>
+    /// <param name="entries">The list of weighted items to use</param>
+    /// <param name="totalWeight">The running total weight</param>
+    /// <returns>A random WeightedItem from the supplied "entries" list</returns>
+private static WeightedItem<T> GetRandomEntry<T>(IEnumerable<WeightedItem<T>> entries, float totalWeight)
+{
+    var threshold = Random.Range(0, totalWeight);
+    var acc = 0f; // accumulator
+    // return the first entry that passes the threshold
+    return entries.FirstOrDefault(entry => (acc += entry.Weight * entry.Limit) >= threshold);
+}
+
+    /// <summary>
+    /// Sets the seed of UnityEngine.Random to the supplied value, if "seed" is not null
+    /// </summary>
+    /// <param name="seed">The seed to init UnityEngine.Random with</param>
+    /// <returns>The state of UnityEngine.Random before setting the seed, null if the supplied seed was null</returns>
+    private static Random.State? SetRngSeed(int? seed)
+    {
+        if (seed == null) return null;
+        
+        var state = Random.state;
+        Random.InitState(seed.Value);
+
+        return state;
     }
 }
--- src/Extensions/Extensions.Tests.cs
+++ src/Extensions/Extensions.Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,42 +9,55 @@ public partial class Extensions
     // Basic tests for GenerateRandomList()
     public static void TestGenerateRandomList()
     {
-        // Basic test with ints
-        var intTest = new List<WeightedItem<int>>
+        try
         {
-            new() { Item = 1, Weight = 100, Limit = 1 },
-            new() { Item = 2, Weight = 50, Limit = 2 },
-            new() { Item = 3, Weight = 25, Limit = 4 }
-        };
-        // 0 & 9 should print an error, all  should generate a random sequence adhering to the limits though
-        for (var i = 0; i < 9; i++)
-        {
-            Plugin.LogSource.LogDebug($"Testing generation of {i} random ints from the list, using seed \"0x1234\"");
-            LogList(GenerateRandomList(intTest, i, 0x1234));
+            // Basic test with ints
+            var intTest = new List<WeightedItem<int>>
+            {
+                new() { Item = 1, Weight = 100, Limit = 1 },
+                new() { Item = 2, Weight = 50, Limit = 2 },
+                new() { Item = 3, Weight = 25, Limit = 4 }
+            };
+            // 0 & 9 should print an error, all should generate a random sequence adhering to the limits though
+            for (var i = 0; i < 9; i++)
+            {
+                Plugin.LogSource.LogDebug($"Testing generation of {i} random ints from the list, using seed \"0x1234\"");
+                LogList(GenerateRandomList(intTest, i, seed: 0x1234));
+            }
+
+            // should be relatively random outputs
+            for (var i = 1; i < 10; i++)
+            {
+                Plugin.LogSource.LogDebug($"Testing generation of 7 random ints from the list:");
+                LogList(GenerateRandomList(intTest, 7));
+            }
+
+            // test with creatures
+            var creatureTest = new List<WeightedItem<CreatureTemplate.Type>>
+            {
+                new() { Item = CreatureTemplate.Type.BlueLizard, Weight = 20, Limit = 5, Contribution = 0.5f },
+                new() { Item = CreatureTemplate.Type.PinkLizard, Weight = 20, Limit = 5, Contribution = 0.5f  },
+                new() { Item = CreatureTemplate.Type.Salamander, Weight = 20, Limit = 5, Contribution = 0.5f  },
+                new() { Item = CreatureTemplate.Type.CyanLizard, Weight = 10, Limit = 3, Contribution = 0.5f  },
+                new() { Item = CreatureTemplate.Type.RedLizard, Weight = 10, Limit = 2, Contribution = 1f  },
+                new() { Item = CreatureTemplate.Type.Vulture, Weight = 10, Limit = 1, Contribution = 2f  }
+            };
+
+            for (var i = 1; i < 10; i++)
+            {
+                Plugin.LogSource.LogDebug($"Testing generation of 10 random creatures:");
+                LogList(GenerateRandomList(creatureTest, 10));
+            }
+
+            for (var i = 1; i < 10; i++)
+            {
+                Plugin.LogSource.LogDebug($"Testing generation of 10 random creatures, with a target of 4.0:");
+                LogList(GenerateRandomList(creatureTest, 10, target: 4));
+            }
         }
-
-        // should be relatively random outputs
-        for (var i = 1; i < 8; i++)
+        catch (Exception e)
         {
-            Plugin.LogSource.LogDebug($"Testing generation of 7 random ints from the list:");
-            LogList(GenerateRandomList(intTest, 7));
-        }
-
-        // test with creatures
-        var creatureTest = new List<WeightedItem<CreatureTemplate.Type>>
-        {
-            new() { Item = CreatureTemplate.Type.BlueLizard, Weight = 20, Limit = 5 },
-            new() { Item = CreatureTemplate.Type.PinkLizard, Weight = 20, Limit = 5 },
-            new() { Item = CreatureTemplate.Type.Salamander, Weight = 20, Limit = 5 },
-            new() { Item = CreatureTemplate.Type.CyanLizard, Weight = 10, Limit = 3 },
-            new() { Item = CreatureTemplate.Type.RedLizard, Weight = 5, Limit = 1 },
-            new() { Item = CreatureTemplate.Type.Vulture, Weight = 5, Limit = 1 }
-        };
-
-        for (var i = 1; i < 8; i++)
-        {
-            Plugin.LogSource.LogDebug($"Testing generation of 10 random creatures:");
-            LogList(GenerateRandomList(creatureTest, 10));
+            Plugin.LogSource.LogError($"GenerateRandomList() Failed testing with error:{e}");
         }
     }
 
