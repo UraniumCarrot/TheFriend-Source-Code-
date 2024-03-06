dd66a520b9bfe51059c9ad86ec7b648989f7461f
NoirCatto <thesoaringcatto@gmail.com>
2024-03-06 20:41:55 +0100

    Setup UnitTest project

---
+++ TheFriendTests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TheFriendTests
+{
+    public class Tests(ITestOutputHelper testConsole)
+    {
+        [Fact]
+        public void Test1()
+        {
+            testConsole.WriteLine("Hello silly world!");
+            Assert.True(true);
+        }
+    }
+}
